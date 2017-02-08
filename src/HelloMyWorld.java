08cdbbe6bdc4003c33de85aea9fa2d6552a088fc
Hanna Zhahala <Hanna_Zhahala@epam.com>
2017-02-08 22:03:43 +0100

    Hello my world

---
+++ src/HelloMyWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Hanna_Zhahala on 08.02.2017.
+ */
+public class HelloMyWorld {
+
+        public static void main(String[] args) {
+            HelloMyWorld text = new HelloMyWorld();
+            System.out.println(text.helloWorld());
+
+        }
+        public String helloWorld (){
+
+            return "Hello World " ;
+
+        }
+    }
+
+
